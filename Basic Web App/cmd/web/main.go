91a297803bb345f6769d455baf75e687eb7ce8a8
karthikbhandary2 <karthikbhandary2@gmail.com>
2025-01-03 10:47:27 +0530

    Basic Web app

---
+++ Basic Web App/cmd/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/karthikbhandary2/bwa/pkg/handlers"
+)
+
+const portNumber = ":8080"
+
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
---
+++ Basic Web App/go.mod
@@ -0,0 +1,3 @@
+module github.com/karthikbhandary2/bwa
+
+go 1.23.2
